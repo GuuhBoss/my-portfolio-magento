4509a186e7a2e4ceecb371041befb3d73153e4fa
guuhboss <gustavo.silva@toogas.com>
2022-12-31 15:28:00 +0000

    start fix

--- app/extra/Magento/Framework/MessageQueue/PublisherPool.php
+++ app/extra/Magento/Framework/MessageQueue/PublisherPool.php
@@ -10,7 +10,11 @@ namespace Magento\Framework\MessageQueue;
  * @license  http://opensource.org/licenses/osl-3.0.php Open Software License OSL3.0
  * @link     http://toogas.com
  */
-class PublisherPool
+class PublisherPool implements PublisherInterface
 {
 
+    public function publish( $topicName, $data )
+    {
+        // TODO: Implement publish() method.
+    }
 }
